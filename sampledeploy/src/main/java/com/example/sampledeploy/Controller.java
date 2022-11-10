b7cc9a94aed2e218cacb918fbfd2d9c1a151ec9f
Basil Roy <basil.k.roy@hotmail.com>
2022-11-10 22:30:11 +0000

    test

--- sampledeploy/src/main/java/com/example/sampledeploy/Controller.java
+++ sampledeploy/src/main/java/com/example/sampledeploy/Controller.java
@@ -1,2 +1,12 @@
-package com.example.sampledeploy;public class Controller {
+package com.example.sampledeploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+  @GetMapping("https://sample-deploy.azurewebsites.net/")
+  public String re(){
+    return "rerere";
+  }
 }
