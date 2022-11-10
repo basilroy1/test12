7549e74c73f37dda32949fd6878d61e0e67839cb
Basil Roy <basil.k.roy@hotmail.com>
2022-11-10 11:51:01 +0000

    test

---
+++ sampledeploy/src/main/java/com/example/sampledeploy/SampledeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampledeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampledeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampledeployApplication.class, args);
+	}
+
+}
